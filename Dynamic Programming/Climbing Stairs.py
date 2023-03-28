8fe6b21fe5ace0560c7916fd47d38bb1b7e5fb6d
dudwnqkr <79184083+qkrwndudata@users.noreply.github.com>
2023-03-28 11:29:14 +0900

    Update Climbing Stairs.py

--- Dynamic Programming/Climbing Stairs.py
+++ Dynamic Programming/Climbing Stairs.py
@@ -12,14 +12,17 @@ class Solution:
                 a, b = b, a+b
         return b
     
-# 다른 풀이: Dynamic programming
+# 다른 풀이: Dynamic programming (memoization)
 def climb(n):
     #edge cases
-    if n==0: return 0
-    if n==1: return 1
-    if n==2: return 2
-    dp = [0]*(n+1) # considering zero steps we need n+1 places
-    dp[1]= 1
+    if n==0: 
+	return 0
+    if n==1: 
+	return 1
+    if n==2:
+	return 2
+    dp = [0] * (n+1) # considering zero steps we need n+1 places
+    dp[1] = 1
     dp[2] = 2
     for i in range(3,n+1):
         dp[i] = dp[i-1] +dp[i-2]
